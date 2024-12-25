d623c80c6f90036b9812f285d412348178c16fc6
Stha07anshu <ansh091rock@gmail.com>
2024-12-25 21:45:53 +0545

    making product

---
+++ models/productModels.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+    productName: {
+        type: String,
+        required: true,
+    },
+    productPrice: {
+        type: Number,
+        required: true,
+    },
+    productDescription: {
+        type: String,
+        required: true,
+        maxlength: 300,
+    },
+    productCategory: {
+        type: String,
+        required: true,
+    },
+    productImage: {
+        type: String,
+        required: true,
+    },
+    productRating: {
+        type: Number,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    }
+});
+
+// Export the model
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = Product;
