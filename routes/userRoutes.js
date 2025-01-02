6c09def0bf32969504a11f24a269687efe68f111
Stha07anshu <ansh091rock@gmail.com>
2025-01-02 16:46:26 +0545

    r

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,11 +4,18 @@ const express = require('express');
 const nodemailer = require('nodemailer');
 const User = require('../models/userModels');
 const userControllers = require('../controllers/userControllers');
+const { authGuard } = require('../middleware/authGuard');
 // Make a create user API
 router.post('/create', userControllers.createUser);
 
 // Login user API
 router.post('/login', userControllers.loginUser)
+
+// Update user details (either by old password or OTP)
+router.post('/update/:id', authGuard, userControllers.updateUser);
+
+// Request OTP for updating user details
+router.post('/request-otp', userControllers.requestOtp);
 
 
 
