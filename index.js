c4529c93743feeddae3166f5f23bfac76444f803
Stha07anshu <ansh091rock@gmail.com>
2025-01-02 16:42:55 +0545

    1

--- index.js
+++ index.js
@@ -48,6 +48,10 @@ app.get('/test', (req,res)=>{
 // Configuring routes
 app.use('/api/user', require('./routes/userRoutes'))
 app.use('/api/product', require('./routes/productRoutes'))
+app.use('/api/order', require('./routes/orderRoutes'))
+app.use('/api/contact', require('./routes/contactRoutes'))
+app.use('/api/cart', require('./routes/cartRoutes'))
+app.use('/api/esewa',require('./routes/esewaRoutes'));
 
 // route reult
 // http://localhost:5000/api/product/create
