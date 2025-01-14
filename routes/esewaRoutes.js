c9de152345a5d62084e2379b0e6484acf39d4644
Stha07anshu <ansh091rock@gmail.com>
2025-01-14 11:00:05 +0545

    esewa

--- routes/esewaRoutes.js
+++ routes/esewaRoutes.js
@@ -2,12 +2,17 @@ var express = require("express");
 const {
   handleEsewaSuccess,
   handleEsewaFailure,
+  createOrder,
 } = require("../controllers/esewaController");
-const { createOrder } = require("../controllers/esewaController");
 var router = express.Router();
- 
+
+// Route to handle the successful payment from Esewa
 router.get("/success", handleEsewaSuccess);
+
+// Route to create a new order and initiate the payment process
 router.post("/create", createOrder);
+
+// Route to handle the failed payment from Esewa
 router.get("/failure", handleEsewaFailure);
- 
+
 module.exports = router;
